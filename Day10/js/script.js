46dc025e44189b204ce07bc20f49972d515807db
itsprinsprays <prncbntz@gmail.com>
2026-06-28 21:23:21 +0800

    Updating and Creating of JSON. Array of JSON

--- Day10/js/script.js
+++ Day10/js/script.js
@@ -16,3 +16,38 @@ let person = {
 console.log(`${person.firstName} ${person.lastName}`);
 console.log(person.hobbies[1]); 
 console.log(person.laptop[1].brand);
+
+//updating value
+person.firstName = "Jediel";
+console.log(`${person.firstName} ${person.lastName}`);
+
+//create new key with value
+person.section = "BSIT-2E";
+console.log(person);
+
+//convert object to string
+let strPerson = JSON.stringify(person);
+console.log(strPerson); 
+
+//convert string to object
+strPerson = JSON.parse(strPerson);
+console.log(strPerson);
+
+
+
+//Array of objects
+
+let people = [
+    {
+        firstName: "Prince",
+        lastName: "Benitez",
+        age: 21
+    },
+    {
+        firstName: "Jediel",
+        lastName: "Benitez",
+        age: 22
+    }
+];
+
+console.log(people[1].firstName);
