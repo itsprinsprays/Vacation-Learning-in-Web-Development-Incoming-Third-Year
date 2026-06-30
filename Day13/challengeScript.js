0dc1ebac7c4c6da4f5e27424e8814ca95ec93303
itsprinsprays <prncbntz@gmail.com>
2026-06-30 16:48:50 +0800

    Tutorial Challenge : Use DOM on styling a web page

---
+++ Day13/challengeScript.js
@@ -0,0 +1,25 @@
+let body = document.querySelector("body");
+body.style.backgroundColor = "rgb(15,15,15)";
+body.style.margin = "0px";
+
+
+let div = document.querySelector(".box");
+div.style.textAlign = "center";
+div.style.backgroundColor = "black";
+div.style.paddingBottom = "15px";
+div.style.paddingTop = "15px";
+
+
+let sdptLogo = document.querySelector("img");
+sdptLogo.src = "https://tse3.mm.bing.net/th/id/OIP.A7uUZUcgygEyPwqeFcPZngAAAA?pid=Api&P=0&h=180";
+
+let title = document.querySelector("#title");
+title.innerHTML = "SDPT ACADEMY";
+title.style.color = "white";
+title.style.fontFamily = "verdana";
+
+let description = document.querySelector("p");
+description.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ratione corporis reiciendis, officiis aspernatur quaerat quo dolorem sint quibusdam. Numquam, accusamus. Nisi facere reiciendis repudiandae maxime incidunt explicabo ab doloribus.";
+description.style.textAlign = "center";
+description.style.color = "white";
+
