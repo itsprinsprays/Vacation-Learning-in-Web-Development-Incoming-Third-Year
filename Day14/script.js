2535812f0bac0c8f01fde649181198f3b981ccbe
itsprinsprays <prncbntz@gmail.com>
2026-07-01 22:11:32 +0800

    DOM Events

---
+++ Day14/script.js
@@ -0,0 +1,45 @@
+//onload
+function bodyloading() {
+    alert("Welcome To my website");
+    let h1 = document.querySelector("h1");
+    h1.style.color = "red";
+}
+
+//onclick
+function buttonText() {
+    let title = document.querySelector("h1");
+    title.innerHTML = "Hello World";
+}
+    
+//onmouseover 
+function hoverText() {
+    let button1 = document.querySelector("#button1"); 
+    button1.style.backgroundColor = "blue";
+}
+
+//onmouseout
+function unhoverText() {
+    let button1 = document.querySelector("#button1"); 
+    button1.style.backgroundColor = "white";
+}
+
+//
+function triggeredFocus() {
+    let input = document.querySelector("#input1");
+    input.style.color = "red";
+    input.style.backgroundColor = "black";
+}
+
+function triggeredBlur() {
+    let input = document.querySelector("#input1");
+    input.style.color = "black";
+    input.style.backgroundColor = "white";
+}
+
+function onInput() {
+    let input2 = document.querySelector("#input2");
+    let output = document.querySelector("p");
+    output.innerHTML = input2.value;
+}
+
+
