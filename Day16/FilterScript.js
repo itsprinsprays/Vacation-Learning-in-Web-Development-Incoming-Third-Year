0f4f68f969067d19ac34c1efa2f6a261c6c80953
itsprinsprays <prncbntz@gmail.com>
2026-07-03 22:16:47 +0800

    Map and Filter

--- Day16/FilterScript.js
+++ Day16/FilterScript.js
@@ -40,13 +40,28 @@ let startsWithP = names.filter(pStart => pStart.startsWith("P"));
 alert(startsWithP);
 
 let people = [
-{ name: "Prince", age: 20 },
-{ name: "John", age: 16 },
-{ name: "Maria", age: 19 },
-{ name: "James", age: 15 },
-{ name: "Anna", age: 25 }
+    { name: "Prince", age: 20 },
+    { name: "John", age: 16 },
+    { name: "Maria", age: 19 },
+    { name: "James", age: 15 },
+    { name: "Anna", age: 25 }
 ];
 
 let adult = people.filter(adults => adults.age > 18);
 
 console.log(adult);
+
+//Filter and Map
+
+let student = [
+{ name: "Prince", score: 95 },
+{ name: "John", score: 60 },
+{ name: "Maria", score: 88 },
+{ name: "James", score: 70 }
+];
+ 
+let passed = student
+		.filter(passing => passing.score >= 75)
+		.map(studentName => studentName.name);
+
+console.log(passed);
