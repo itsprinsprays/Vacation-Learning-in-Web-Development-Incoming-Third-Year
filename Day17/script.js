050a57985ba51d272f0f831d774c8608d350a18f
itsprinsprays <prncbntz@gmail.com>
2026-07-05 22:22:33 +0800

    Async/Await and try catch

--- Day17/script.js
+++ Day17/script.js
@@ -1,17 +1,55 @@
-function waitTwoSeconds() {
-    return new Promise(resolve => {
+// function waitTwoSeconds() {
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//             resolve('Finished waiting!')
+//         }, 2000);
+//     });
+// }
+
+// async function  start() {
+// console.log("start");
+
+// let result = await waitTwoSeconds();
+// console.log(result);
+// console.log("end");
+
+// }
+
+// // start();
+
+// async function getPost() {
+//   console.log("Fetching post...");
+//   let response = await fetch("https://jsonplaceholder.typicode.com/posts/30");
+//   let data = await response.json();
+//   console.log("Post title:", data.title);
+// }
+
+// getPost();
+
+
+function login(){
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve('Finished waiting!')
+
+            let username = prompt("Enter username: ");
+            let password = prompt("Enter a password: ");
+                if(username === "Prince" && password === "12345") {
+                  resolve(`Welcome ${username}!`)
+              
+                } else {
+                    reject("Invalid Username or password");
+                }
         }, 2000);
     });
 }
 
-async function  start() {
-console.log("start");
-
-let result = await waitTwoSeconds();
-console.log(result);
-console.log("end");
+async function start() {
+    try {
+        const logins = await login();
+        document.querySelector("#login").innerHTML = logins;
+    } catch(error) {
+        document.querySelector("#login").innerHTML = error;
+    }
 
 }
 
