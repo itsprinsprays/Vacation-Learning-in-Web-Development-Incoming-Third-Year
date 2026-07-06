7614f8b444adfdeb7ef2908b7b0fb3516daa7489
itsprinsprays <prncbntz@gmail.com>
2026-07-06 20:19:54 +0800

    Challenge Done

--- Day17/employee.js
+++ Day17/employee.js
@@ -1,13 +1,14 @@
 function getEmployee(id) {
     return new Promise((resolve) => {
         setTimeout(() => {
-            const employee = [
-                {id: 1, name: "Prince", departmentId: 102},
-                {id: 2, name: "John", departmentId: 101}
-            ];
+            const employees = [
+                    { id: 1, name: "Prince", departmentId: 100 },
+                    { id: 2, name: "John", departmentId: 101 },
+                    { id: 3, name: "Mary", departmentId: 102 }
+                ];
             
-           const get = employee
-                        .filter(gets => gets.id === id);
+           const get = employees
+                        .find(gets => gets.id === id);
             resolve(get);
 
         }, 2000);
@@ -17,12 +18,14 @@ function getEmployee(id) {
 function getDepartment(id) {
     return new Promise((resolve) => {
         setTimeout(() => {
-            const department = [
-                {id: 101, name: "IT Department"}
-            ];
+            const departments = [
+                    { id: 100, name: "HR" },
+                    { id: 101, name: "IT Department" },
+                    { id: 102, name: "Finance" }
+                ];
 
-            const get = department
-                        .filter(gets => gets.id === id);
+            const get = departments
+                        .find(gets => gets.id === id);
             resolve(get);
         }, 3000);
     });
@@ -33,10 +36,11 @@ async function start() {
     load.innerHTML = "Loading employee...";
     const employee = await getEmployee(2);
     console.log(employee);
-    console.log(employee.departmentId);
 
     const department = await getDepartment(employee.departmentId);
     console.log(department);
+    console.log("Employee:" +employee.name);
+    console.log("Department: " +department.name);
 }
 
 start();
