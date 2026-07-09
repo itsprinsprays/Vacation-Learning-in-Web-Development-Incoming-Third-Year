a24bdf55fb9d713f44501cb84619fc0189455ae0
itsprinsprays <prncbntz@gmail.com>
2026-07-09 21:16:14 +0800

    Fetch, Async and Await

--- Day18/script.js
+++ Day18/script.js
@@ -4,7 +4,28 @@ async function getUSers() {
 
     const users = await response.json();
 
-    console.log(users[1].name);
+    console.log(users);
 }
 
 getUSers();
+
+async function getUserById(Id) {
+
+    const response = await fetch ("https://jsonplaceholder.typicode.com/users");
+
+    const user = await response.json();
+    const filteredUser = user
+                        .find(finds => finds.id === Id);
+
+    return filteredUser;
+
+}
+
+async function start() {
+    const user = await getUserById(1);
+
+    console.log(user.name);
+}
+
+start();
+
