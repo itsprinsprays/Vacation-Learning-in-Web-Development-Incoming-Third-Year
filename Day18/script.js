59d7be631b4512cc608a13698214c481a5e7981e
itsprinsprays <prncbntz@gmail.com>
2026-07-10 23:02:33 +0800

    Added validation

--- Day18/script.js
+++ Day18/script.js
@@ -9,22 +9,22 @@ async function getUSers() {
 
 getUSers();
 
-async function getUserById(Id) {
+async function getUserById(id) {
 
     const response = await fetch ("https://jsonplaceholder.typicode.com/users");
 
     const user = await response.json();
     const filteredUser = user
-                        .find(finds => finds.id === Id);
-
+                        .find(finds => finds.id === id);
     return filteredUser;
 
 }
 
 async function start() {
-    const user = await getUserById(1);
+    const user = await getUserById(100);
 
-    console.log(user.name);
+    if(user === undefined) console.log("User not found.");
+    else console.log(user.name);
 }
 
 start();
