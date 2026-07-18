99286c673bca7efaf9cb0f629f712711559e01ce
itsprinsprays <prncbntz@gmail.com>
2026-07-18 21:35:42 +0800

    App.jsx added

---
+++ Day20/App.jsx
@@ -0,0 +1,11 @@
+import CreateCourse from "./components/CreateCourse";
+
+function app() {
+    return (
+        <div>
+            <CreateCourse />
+        </div>
+    )
+}
+
+export default App;
