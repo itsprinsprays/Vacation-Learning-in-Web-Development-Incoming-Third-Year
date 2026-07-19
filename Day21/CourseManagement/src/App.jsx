e559b3a0b780440fdcceac61d0028b8362a12575
itsprinsprays <prncbntz@gmail.com>
2026-07-19 13:22:27 +0800

    Header, Nav, and Footer Added

--- Day21/CourseManagement/src/App.jsx
+++ Day21/CourseManagement/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
-
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 function App() {
   return (
+    <>
+    <Header />
     <div>
       <h1>Course Management System</h1>
       <p>Welcome to my first React application!</p>
     </div>
+    <Footer />
+  </>
   )
 }
 
---
+++ Day21/CourseManagement/src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <h2 className ="logo">Course Management</h2>
+
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+            </nav>
+        </header>
+    )
+}
+ 
