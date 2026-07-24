bca87592ee62d08ebf04094b312e99709aa4b9e0
itsprinsprays <prncbntz@gmail.com>
2026-07-24 20:05:24 +0800

    Learning useState

--- Day21/CourseManagement/src/App.jsx
+++ Day21/CourseManagement/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
 import Main from "./components/Main/Main"
+import Counter from "./components/Counter"
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <h1>Course Management System</h1>
       <p>Welcome to my first React application!</p>
       <Main name="Prince Benitez" age={21}/>
+
+      <Counter />
     </div>
     <Footer />
   </>
