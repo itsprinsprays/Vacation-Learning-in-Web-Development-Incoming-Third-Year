7bbe978f069010a85426bc5db13f7493dcb87c4f
itsprinsprays <prncbntz@gmail.com>
2026-07-24 21:18:48 +0800

    Experimenting on useState

--- Day21/CourseManagement/src/components/Counter.jsx
+++ Day21/CourseManagement/src/components/Counter.jsx
@@ -2,14 +2,24 @@ import { useState } from "react";
 
 export default function Counter() {
 
+    const studentNames = ["Prince", "Jedi", "Ben", "Snirp"];
+
     const [count, setCount] = useState(0);
+    const [name, setName] = useState("");
 
     return (
         <>
 
         <h1>{count}</h1>
+        <h1>{name}</h1>    
+
         
-        <button onClick={() => setCount(count + 2)}> Increase</button>
+        <button onClick={() => {
+            if(count < studentNames.length) {
+                setCount(count + 1);
+                setName(studentNames[count]);
+            } else alert("No Student Ahead!");
+        }}> Increase</button>
 
         </>
     );
